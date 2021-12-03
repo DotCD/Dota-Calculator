95d61a7d51e7635f7d5031e7d3e06f15ef36e88b
DotCD <dotcdtg@gmail.com>
2021-12-02 21:02:12 -0500

    Better animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,15 @@ export class AppComponent {
   title = 'dota-calc';
 
   advanced: boolean = false;
+  display: boolean = true;
   hero: Map<string, Map<string, number>> = new Map();
 
   updateHero(hero: Map<string, Map<string, number>>) {
     this.hero = hero;
+  }
+
+  change() {
+    this.display = false;
+    setTimeout(() => {this.display = true;}, 0);
   }
 }
